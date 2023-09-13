29ce8dbebfb420f25e013ac422afa54bd2e4fc3d
Fernando Guerrero <devrrior@gmail.com>
2023-09-12 23:15:48 -0600

    feat(Project): Set up spring boot project

---
+++ src/main/java/com/example/pharmacyapidemo/PharmacyApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyApiDemoApplication.class, args);
+    }
+
+}
